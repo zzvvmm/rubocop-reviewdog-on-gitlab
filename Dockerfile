0c7a2a2679b04a0061b5a50668780faf5be40e3c
DD <zzvvmm96@gmail.com>
2022-12-16 00:18:33 +0700

    reviewdog with RuboCop on GitlabCI

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ruby:2.7
+USER root
+
+RUN mkdir ~/.ssh && touch ~/.ssh/known_hosts
+RUN apt-get update -y && apt-get install -y curl rubygems
+RUN curl -sfL https://raw.githubusercontent.com/reviewdog/nightly/master/install.sh | sh -s -- -b /usr/bin
+RUN gem install standard rubocop-rails
---
+++ run-reviewdog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo '::group::🐶 Fetching changed files'
+file_name=$(git diff origin/master HEAD --name-only -- `find . -name '*.rb'`| tr '\n' ' ') # change origin/master to your main branch (important!)
+echo '::endgroup::'
+
+echo '::group:: Running rubocop with reviewdog 🐶 ...'
+rubocop $file_name --require /opt/rdjson_formatter.rb --format RdjsonFormatter \
+  | reviewdog \
+    -f=rdjson \
+    -reporter="gitlab-mr-discussion" \
+    -filter-mode="added" \
+    -fail-on-error=false \
+
+reviewdog_rc=$?
+echo '::endgroup::'
+
+exit $reviewdog_rc
